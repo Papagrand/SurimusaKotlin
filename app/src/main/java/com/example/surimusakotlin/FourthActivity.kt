ae49d6993f490c3681375ee5c9a03cbc39c2bad5
Nikolay <horov.kolya@yandex.ru>
2024-03-29 18:50:30 +0300

    -Refactor progress_fragment.xml -bottomNavigation added -SearchView added -darkMode theme added

--- app/src/main/java/com/example/surimusakotlin/FourthActivity.kt
+++ app/src/main/java/com/example/surimusakotlin/FourthActivity.kt
@@ -1,6 +1,7 @@
 package com.example.surimusakotlin
 
 import android.os.Bundle
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,11 +9,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FourthActivity : AppCompatActivity() {
+
+    private lateinit var searchView: SearchView
+    private var searchText: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product_or_meal)
         val meal = intent.getStringExtra("meal")
         val breakfastLunchDinnerTextView = findViewById<TextView>(R.id.breakfast_lunch_dinner)
         breakfastLunchDinnerTextView.text = meal
+
+
+        searchView = findViewById(R.id.search_view)
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString("searchText")
+            searchView.setQuery(searchText, false)
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchText = newText
+                return true
+            }
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("searchText", searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString("searchText")
     }
 }
--- app/src/main/java/com/example/surimusakotlin/MainActivity.kt
+++ app/src/main/java/com/example/surimusakotlin/MainActivity.kt
@@ -1,25 +1,51 @@
 package com.example.surimusakotlin
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        replaceFragment(ProgressFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_progress -> {
+                    replaceFragment(ProgressFragment())
+                    true
+                }
+                R.id.bottom_recepies -> {
+                    replaceFragment(RecepiesFragment())
+                    true
+                }
+                R.id.bottom_flasting -> {
+                    replaceFragment(FlastingFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-    // Метод для обработки нажатия на кнопку для перехода на SecondActivity
-    fun goToSecondActivity(view: View) {
-        val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 
-    // Метод для обработки нажатия на кнопку для перехода на ThirdActivity
-    fun goToThirdActivity(view: View) {
-        val intent = Intent(this, ThirdActivity::class.java)
-        startActivity(intent)
-    }
 }
